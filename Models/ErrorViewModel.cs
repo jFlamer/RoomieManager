b635574b08dfd4e54032e4dcdaad0c2982bf11c9
Jagoda Flejmer <jflejmer@student.agh.edu.pl>
2025-05-17 13:53:38 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RoomieManager.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
