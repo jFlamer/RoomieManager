d4a06a01c63dbfe3ce8a6d677fb1515e4edda233
jFlamer <jflejmer@student.agh.edu.pl>
2025-05-22 12:59:03 +0200

    updated login an register

--- DBInitializer.cs
+++ DBInitializer.cs
@@ -10,7 +10,13 @@ namespace RoomieManager.Data
             {
                 return; // DB has been seeded
             }
-
+            var admin = new UserModel
+            {
+                userName = "admin",
+                password = "21232F297A57A5A743894A0E4A801FC3", // MD5 for 'admin'
+                isAdmin = true
+            };
+            context.Users.Add(admin);
             context.SaveChanges();
         }
     }
