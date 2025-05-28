75839835307c7c434163397151b10f694c6250bc
Jagoda Flejmer <jflejmer@student.agh.edu.pl>
2025-05-28 12:22:47 +0200

    effort points and view

---
+++ Migrations/20250527155943_UpdateTaskType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoomieManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTaskType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "effortPoints",
+                table: "TaskTypes",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "effortPoints",
+                table: "TaskTypes");
+        }
+    }
+}
